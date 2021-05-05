16f63ac85dca61ee664df24399a64fb6672c1229
Fojan Babaali <fozhanmail@gmail.com>
2021-05-04 19:42:34 -0700

    Update App.js

--- weather-app/src/containers/App/App.js
+++ weather-app/src/containers/App/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import "./App.css";
 const api_key = {
-  api_key : "4b3e61d509a14cd79c90d406e79d5d1a",
-  base_api_url :"https://api.openweathermap.org/data/2.5/"
-
-}
+  api_key: "4b3e61d509a14cd79c90d406e79d5d1a",
+  base_api_url: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
   return (
-    <div>hi</div>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <form>
+            <input type="text" onChange="" className="serach-bar" placeholder="search..."></input>
+            <button type="submit" onClick="" className="search-button">
+              serach
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 }
 

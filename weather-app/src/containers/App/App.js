9410ff6a1c8c4bca51347ebb12841f1f23b8fed1
Fojan Babaali <fozhanmail@gmail.com>
2021-05-04 19:05:31 -0700

    added

---
+++ weather-app/src/containers/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+const api_key = {
+  api_key : "4b3e61d509a14cd79c90d406e79d5d1a",
+  base_api_url :"https://api.openweathermap.org/data/2.5/"
+
+}
+function App() {
+  return (
+    <div>hi</div>
+  );
+}
+
+export default App;

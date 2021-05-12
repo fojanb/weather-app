a3872413490db9d5b1a0019b482e034ac61e1eff
Fojan Babaali <fozhanmail@gmail.com>
2021-05-11 22:41:19 -0700

    Update Dt.js

--- weather-app/src/Dt.js
+++ weather-app/src/Dt.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Dt.css";
 const Dt = (props) => {
-    let dateObj = new Date();
-    let hours = dateObj.toUTCstring(props.weatherDt).slice(-11,-4);
+  const convertTimestamptoTime = (props) => {
+    let unixTimestamp = props.weatherDt;
+    // convert to milliseconds and
+    // then create a new Date object
+    let dateObj = new Date(unixTimestamp * 1000);
+    let utcString = dateObj.toUTCString();
+
+    let time = utcString.slice(-11, -4);
+    return time;
+  };
   return (
     <div className="weatherDt-box">
-      <div className="dt">Time of Calculations : {hours}</div>
+      <div className="dt">
+        Time of Calculations : {convertTimestamptoTime(props)}
+      </div>
     </div>
   );
 };

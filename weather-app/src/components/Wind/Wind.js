f35ec8c24c7b146b99ee47c7ae5ff673a3a06747
Fojan Babaali <fozhanmail@gmail.com>
2021-05-16 23:04:38 -0700

    Update Wind.js

--- weather-app/src/components/Wind/Wind.js
+++ weather-app/src/components/Wind/Wind.js
@@ -5,6 +5,8 @@ const Wind = (props) => {
     <div className="weatherWind-box">
       <div className="wind">
         <div>Wind-Speed {props.weatherWind.speed} meter / sec</div>
+        <img src="https://img.icons8.com/material/20/ffffff/speed--v2.png" />
+
         <div>Wind-Direction {props.weatherWind.deg} deg</div>
       </div>
     </div>

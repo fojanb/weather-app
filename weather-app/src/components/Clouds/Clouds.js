66343f4d4807f6eba2739a98a6c80c9a5f1e1ec4
Fojan Babaali <fozhanmail@gmail.com>
2021-05-16 18:09:51 -0700

    Update Clouds.js

--- weather-app/src/components/Clouds/Clouds.js
+++ weather-app/src/components/Clouds/Clouds.js
@@ -3,7 +3,10 @@ import "./Clouds.css";
 const Clouds = (props) => {
   return (
     <div className="weatherClouds-box">
-      <div className="clouds">Cloudiness {props.weatherClouds.all}%</div>
+      <div className="clouds">
+        Cloudiness {props.weatherClouds.all}%{" "}
+        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/partly-cloudy-day--v1.png" />
+      </div>
     </div>
   );
 };
